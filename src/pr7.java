4a50e6088ef9e117d178c43e0f9a469388247ac9
Tomascod <tomas.kodydek@infis.cz>
2026-01-14 08:35:33 +0100

    hotovo - zkontrolovat2

--- src/pr7.java
+++ src/pr7.java
@@ -1,7 +1,7 @@
 public class pr7 {
     public static void main(String[] args) {
-        String regex = "([0-9]|1[0-9]|2[0-9]|3[01])\\.([0-9]|1[0-2])\\.\\d{4}";
-        String date = "31.11.2025";
+        String regex = "([1-9]|1[0-9]|2[0-9]|3[01])\\.([1-9]|1[0-2])\\.\\d{4}";
+        String date = "16.10.2025";
 
         if (date.matches(regex)) System.out.println("Správně"); else System.out.println("špatně");
     }
